8fcc1750838801c58efb5a8051e3dd3290b86cfe
rhdzmota <rhdzmota@gmail.com>
2020-10-23 11:10:24 -0500

    HW DEF Sic Industry quickfix

--- src/homework/sic-industry/settings.py
+++ src/homework/sic-industry/settings.py
@@ -3,7 +3,7 @@ import os
 
 INDUSTRY_SEARCH_DEFAULT_FILENAME = os.environ.get(
     "INDUSTRY_SEARCH_DEFAULT_FILENAME",
-    default="./industry-search/industries.json"
+    default="./sic-industry/industries.json"
 )
 
 INDUSTRY_SEARCH_DEFAULT_THRESHOLD = float(os.environ.get(
