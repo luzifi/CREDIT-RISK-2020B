fde766b56330bab08422646ffba1e89f2253adcd
Luz <if708591@iteso.mx>
2020-11-17 23:29:08 -0600

    f

--- src/homework/amortization/main.py
+++ src/homework/amortization/main.py
@@ -7,11 +7,15 @@ class Main:
 
     @staticmethod
     def annuity(amount: float, rate: float, n: int):
-        raise NotImplementedError
+        f = Amortization(amount, rate, n)
+        res = f.annuity
+        return res
 
     @staticmethod
     def table(amount: float, rate: float, n: int, save_file: Optional[str] = None):
-        raise NotImplementedError
+        ta = Amortization(amount, rate, n)
+        tt = ta.get_table(save_file)
+        return tt
 
     @staticmethod
     def plot(amount: float, rate: float, n: int, save_file: Optional[str] = None):
